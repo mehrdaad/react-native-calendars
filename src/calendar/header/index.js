8c6bab947d1ad031dd33d9c6d02ea3fe15333d82
Mehrdad Shokri <code.poet95@gmail.com>
2018-08-03 03:18:25 +0430

    Bug fixes

--- src/calendar/header/index.js
+++ src/calendar/header/index.js
@@ -32,8 +32,8 @@ class CalendarHeader extends Component {
     this.style = styleConstructor(props.theme, props.type === 'jalaali');
     this.addMonth = this.addMonth.bind(this);
     this.substractMonth = this.substractMonth.bind(this);
-    this.onPressLeft = this.onPressRight.bind(this);
-    this.onPressRight = this.onPressLeft.bind(this);
+    this.onPressLeft = this.onPressLeft.bind(this);
+    this.onPressRight = this.onPressRight.bind(this);
     this.getDefaultFormat = this.getDefaultFormat.bind(this);
     this.state.monthFormat = this.getDefaultFormat(props.monthFormat);
   }
